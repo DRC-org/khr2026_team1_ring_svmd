1902988c3af921ab4fe74303fe0587ce9e72f208
Taiga Saito <me@taigasaito.org>
2026-03-04 14:46:47 +0900

    feat: 完成？

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 #include <Servo.h>
 
 // void readSwitch();
-void updateServos();
+void updatePosServos();
+void updateHandServo();
 
 // サーボ関係
 #define SV0 3   // 関節 1 (150 kg)
 #define SV1 6   // 関節 2 (20 kg)
 #define SV2 10  // ハンド
-#define MSW 9   // 本来は SV3; マイクロ SW を接続するのに使う
+#define SV3 9
 // LED 関係
 #define LED_PIN 13
 #define RGB 8
@@ -33,13 +34,20 @@ unsigned int CAN_ID = 0x401;  // CANのID（サーボモータドライバは 0x
 
 const float SERVO_MAX_SPEED = 5.0f;  // 最大角速度（度/step）
 const float SERVO_ACCEL = 0.4f;      // 加速度（度/step²）
+const float SERVO_LINEAR_SPEED = 5.0f;
 
 Servo* servos[3] = {&servo0, &servo1, &servo2};
 float currentAngles[3] = {80.0f, 80.0f, 80.0f};
 float targetAngles[3] = {80.0f, 80.0f, 80.0f};
 float servoVelocities[3] = {0.0f, 0.0f, 0.0f};
-int8_t states[3] = {2, 2,
-                    2};  // OPEN=0,CLOSE=1,STOPPED=2,OPEN_DONE=3,CLOSE_DONE=4
+
+// 0: PICKUP, 1: YAGURA, 2: HONMARU, 3: STOPPED, 4: PICKUP_DONE, 5: YAGURA_DONE,
+// 6: HONMARU_DONE
+int8_t pos_state = 0;
+
+// 0: OPEN, 1: CLOSE, 2: STOPPED, 3: OPEN_DONE, 4: CLOSE_DONE
+int8_t hand_state = 0;
+
 unsigned long lastUpdateMs = 0;
 
 void setup() {
@@ -48,7 +56,7 @@ void setup() {
   pinMode(SV0, OUTPUT);
   pinMode(SV1, OUTPUT);
   pinMode(SV2, OUTPUT);
-  pinMode(MSW, INPUT_PULLUP);
+  // pinMode(SV3, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(RGB, OUTPUT);
   pinMode(SW0, INPUT_PULLUP);
@@ -101,34 +109,53 @@ void loop() {
 
     if (rxId == CAN_ID) {
       unsigned char command = msg.data[0];
+      unsigned char param = msg.data[1];
 
-      if (command == 0x00) {  // ハンド 1 を閉じる
-        targetAngles[0] = 50.0f;
-        states[0] = 1;               // CLOSE
-      } else if (command == 0x01) {  // ハンド 1 を開く
-        targetAngles[0] = 0.0f;
-        states[0] = 0;               // OPEN
-      } else if (command == 0x02) {  // ハンド 1 を停止
-        Serial.println("STOP HAND 1");
+      if (command == 0x00) {  // ハンド 1 を移動
+        if (param == 0x00) {  // ピックアップ
+          targetAngles[0] = 12.0f;
+          targetAngles[1] = 0.0f;
+          pos_state = 0;
+        } else if (param == 0x01) {  // やぐら
+          targetAngles[0] = 12.0f;
+          targetAngles[1] = 0.0f;
+          pos_state = 1;
+        } else if (param == 0x02) {  // 本丸
+          targetAngles[0] = 12.0f;
+          targetAngles[1] = 0.0f;
+          pos_state = 2;
+        }
+      } else if (command == 0x01) {  // ハンド 1 を停止
         targetAngles[0] = currentAngles[0];
-        servoVelocities[0] = 0.0f;
-        states[0] = 2;  // STOPPED
+        targetAngles[1] = currentAngles[1];
+        pos_state = 3;
+      } else if (command == 0x10) {  // ハンド 1 を閉じる
+        targetAngles[2] = 80.0f;
+        hand_state = 1;
+      } else if (command == 0x11) {  // ハンド 1 を開く
+        targetAngles[2] = 0.0f;
+        hand_state = 0;
+      } else if (command == 0x12) {  // ハンド 1 を停止
+        targetAngles[2] = currentAngles[2];
+        servoVelocities[2] = 0.0f;
+        hand_state = 2;
       }
 
-      if (command == 0x10) {  // ハンド 2 を閉じる
-        targetAngles[1] = 80.0f;
-        states[1] = 1;               // CLOSE
-      } else if (command == 0x11) {  // ハンド 2 を開く
-        targetAngles[1] = 0.0f;
-        states[1] = 0;               // OPEN
-      } else if (command == 0x12) {  // ハンド 2 を停止
-        targetAngles[1] = currentAngles[1];
-        servoVelocities[1] = 0.0f;
-        states[1] = 2;  // STOPPED
-      }
+      // if (command == 0x10) {  // ハンド 2 を閉じる
+      //   targetAngles[1] = 80.0f;
+      //   states[1] = 1;               // CLOSE
+      // } else if (command == 0x11) {  // ハンド 2 を開く
+      //   targetAngles[1] = 0.0f;
+      //   states[1] = 0;               // OPEN
+      // } else if (command == 0x12) {  // ハンド 2 を停止
+      //   targetAngles[1] = currentAngles[1];
+      //   servoVelocities[1] = 0.0f;
+      //   states[1] = 2;  // STOPPED
+      // }
     }
 
-    updateServos();
+    updatePosServos();
+    updateHandServo();
   }
 }
 
@@ -156,10 +183,10 @@ void loop() {
 //   Serial.println(CAN_ID);
 // }
 
-void updateServos() {
+void updatePosServos() {
   for (int i = 0; i < 2; i++) {
-    if (states[i] == 2 || states[i] == 3 || states[i] == 4) {
-      servoVelocities[i] = 0.0f;
+    if (pos_state == 3 || pos_state == 4 || pos_state == 5 || pos_state == 6) {
+      targetAngles[i] = currentAngles[i];
       continue;
     }
 
@@ -170,24 +197,25 @@ void updateServos() {
     if (dist < 0.5f) {
       currentAngles[i] = targetAngles[i];
       servoVelocities[i] = 0.0f;
-      if (states[i] == 0) {
-        states[i] = 3;  // OPEN_DONE
-      } else if (states[i] == 1) {
-        states[i] = 4;  // CLOSE_DONE
-      }
-
-      CanMsg msg;
-      msg.id = 0x000;
-      msg.data_length = 8;
 
       if (i == 0) {
-        msg.data[0] = 0x40;
-      } else if (i == 1) {
-        msg.data[0] = 0x41;
-      }
-      msg.data[1] = states[i] == 3 ? 0x01 : 0x00;
+        if (pos_state == 0) {
+          pos_state = 4;  // PICKUP_DONE
+        } else if (pos_state == 1) {
+          pos_state = 5;  // YAGURA_DONE
+        } else if (pos_state == 2) {
+          pos_state = 6;  // HONMARU_DONE
+        }
 
-      CAN.write(msg);
+        CanMsg msg;
+        msg.id = 0x000;
+        msg.data_length = 8;
+
+        msg.data[0] = 0x4A;
+        msg.data[1] = pos_state == 4 ? 0x00 : (pos_state == 5 ? 0x01 : 0x02);
+
+        CAN.write(msg);
+      }
     } else {
       float dir = (diff > 0.0f) ? 1.0f : -1.0f;
 
@@ -222,4 +250,33 @@ void updateServos() {
 
     servos[i]->write((int)roundf(currentAngles[i]));
   }
+}
+
+void updateHandServo() {
+  if (hand_state == 2 || hand_state == 3 || hand_state == 4) {
+    return;
+  }
+
+  float diff = targetAngles[2] - currentAngles[2];
+  if (fabsf(diff) < 0.5f) {
+    currentAngles[2] = targetAngles[2];
+    if (hand_state == 0) {
+      hand_state = 3;  // OPEN_DONE
+    } else if (hand_state == 1) {
+      hand_state = 4;  // CLOSE_DONE
+    }
+
+    CanMsg msg;
+    msg.id = 0x000;
+    msg.data_length = 8;
+
+    msg.data[0] = 0x4B;
+    msg.data[1] = hand_state == 3 ? 0x01 : 0x00;
+
+    CAN.write(msg);
+  } else {
+    float step = constrain(diff, -SERVO_LINEAR_SPEED, SERVO_LINEAR_SPEED);
+    currentAngles[2] += step;
+  }
+  servos[2]->write((int)roundf(currentAngles[2]));
 }
