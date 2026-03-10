23881ffd3aa7b7099754cc215f1cb4281df799cc
Pens-1 <d0193480501@gmail.com>
2026-03-10 20:02:50 +0900

    値と調整

--- src/config.h
+++ src/config.h
@@ -14,11 +14,11 @@ struct BoardConfig {
 
 // 後基板（CAN 0x401, ID=1）の絶対値
 const BoardConfig CONFIG_401 = {
-  199.0f, 270.0f,  // pickup  SV0, SV2
+  197.0f, 270.0f,  // pickup  SV0, SV2
    40.0f,  50.0f,  // yagura  SV0, SV2
    25.0f,  80.0f,  // honmaru SV0, SV2
     0.0f,  85.0f,  // yagura hand open, close
-  170.0f,  65.0f, 115.0f,  // hand open, close, grip
+  170.0f,  60.0f, 115.0f,  // hand open, close, grip
 };
 
 // 前基板（CAN 0x400, ID=0）のサーボごとオフセット（401の値に何度足すか）
