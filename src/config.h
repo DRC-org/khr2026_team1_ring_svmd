9f464c0f073794fe1b19a115508b06b4a264657d
Taiga Saito <me@taigasaito.org>
2026-03-12 12:23:01 +0900

    format

--- src/config.h
+++ src/config.h
@@ -14,15 +14,15 @@ struct BoardConfig {
 
 // 後基板（CAN 0x401, ID=1）の絶対値
 const BoardConfig CONFIG_401 = {
-  197.0f, 272.0f,  // pickup  SV0, SV2
-   40.0f,  50.0f,  // yagura  SV0, SV2
-   25.0f,  80.0f,  // honmaru SV0, SV2
-    0.0f,  85.0f,  // yagura hand open, close
-  170.0f,  50.0f, 115.0f,  // hand open, close, grip
+    197.0f, 272.0f,          // pickup  SV0, SV2
+    40.0f,  50.0f,           // yagura  SV0, SV2
+    25.0f,  80.0f,           // honmaru SV0, SV2
+    0.0f,   85.0f,           // yagura hand open, close
+    170.0f, 60.0f,  115.0f,  // hand open, close, grip
 };
 
 // 前基板（CAN 0x400, ID=0）のサーボごとオフセット（401の値に何度足すか）
 struct ServoOffsets {
   float sv0, sv2, sv3, sv1;
 };
-const ServoOffsets OFFSETS_400 = { -3.0f, 4.0f, 20.0f, 0.0f };
+const ServoOffsets OFFSETS_400 = {-3.0f, 4.0f, 20.0f, 0.0f};
